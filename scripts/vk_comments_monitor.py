86c53108a4a25ddfac45947e469054eb2fe014a9
toro <timofey89rus@gmail.com>
2025-11-01 11:18:47 +0300

    add full redis storage for posts_id and user/group names

--- scripts/vk_comments_monitor.py
+++ scripts/vk_comments_monitor.py
@@ -8,28 +8,12 @@ from src.tg import send_vk_comments
 logger = logger.getChild("vk_comments")
 
 logger.info("SCRIPT STARTED")
-with open(LATEST_VK_COMMENTS_FILE_PATH, "r") as file:
-    try:
-        latest_vk_comments = json.load(file)
-        logger.info("Latest comments loaded")
-    except json.decoder.JSONDecodeError:
-        logger.info("Latest comments file is empty")
-        latest_vk_comments = {}
 
-
-new_vk_comments = vk_comments_parser.get_new_comments(latest_vk_comments)
+new_vk_comments = vk_comments_parser.get_new_comments()
 if new_vk_comments:
     logger.info("New comments found")
     send_vk_comments(new_vk_comments)
     logger.info("New comments sent")
-    updated_latest_comments = vk_comments_parser.update_latest_comments(
-        latest_vk_comments, new_vk_comments
-    )
-    logger.info("Latest comments updated")
-
-    with open(LATEST_VK_COMMENTS_FILE_PATH, "w") as file:
-        json.dump(updated_latest_comments, file)
-        logger.info("Latest comments saved")
 else:
     logger.info("No new comments found")
 logger.info("SCRIPT ENDED")
--- src/vk_comments_parser.py
+++ src/vk_comments_parser.py
@@ -13,14 +13,12 @@ vk_session = vk_api.VkApi(token=VK_TOKEN)
 vk = vk_session.get_api()
 
 
-def main(latest_saved_posts):
+def main():
     logger.info("Started new comments collecting")
     recent_posts = get_recent_posts_with_comments()
     new_comments = []
     for post in recent_posts:
-        post_new_comments = collect_new_comments_for_post(
-            post, latest_saved_posts
-        )
+        post_new_comments = collect_new_comments_for_post(post)
         if post_new_comments:
             new_comments.append(post_new_comments)
     logger.info(f"Collected {len(new_comments)} posts with new comments")
@@ -55,17 +53,12 @@ def get_recent_posts_with_comments(
     return recent_posts_with_comments
 
 
-def collect_new_comments_for_post(
-    post, latest_saved_posts, group_id=VK_GROUP_ID
-):
+def collect_new_comments_for_post(post, group_id=VK_GROUP_ID):
     post_id = post["id"]
     post_text = f"{post["text"][:100]}..."
     post_date = date.fromtimestamp(post["date"])
     post_new_comments = []
-    if str(post_id) in latest_saved_posts:
-        last_saved_comment_id = latest_saved_posts[str(post_id)]["comment_id"]
-    else:
-        last_saved_comment_id = 0
+    last_saved_comment_id = int(r.get(f"posts:last:comment:{post_id}") or 0)
     post_comments_response = safe_vk_call(
         vk.wall.getComments,
         owner_id=group_id,
@@ -77,15 +70,18 @@ def collect_new_comments_for_post(
     if not post_comments_response:
         return []
     post_comments = post_comments_response["items"]
-    for post_comment in post_comments:
-        print(post_comment["id"])
-        new_comment = process_comment(post_comment)
+    if last_saved_comment_id and post_comments:
+        post_comments = post_comments[1:]
+    for comment in post_comments:
+        new_comment = process_comment(comment)
         if new_comment:
             post_new_comments.append(new_comment)
     if post_new_comments:
         logger.info(
             f"Collected post with {len(post_new_comments)} new comments"
         )
+        last_comment_id = post_new_comments[-1]["comment_id"]
+        r.set(f"posts:last:comment:{post_id}", last_comment_id, ex=604800)
         return {
             "post_id": post_id,
             "post_date": post_date,
@@ -111,30 +107,25 @@ def process_comment(comment):
 def get_author(author_id):
     if author_id < 0:
         group_id = abs(author_id)
+        cached_name = r.get(f"groups:names:{str(group_id)}")
+        if cached_name:
+            return cached_name
         group_response = safe_vk_call(vk.groups.getById, group_id=group_id)
         if not group_response:
             return "Неизвестное сообщество"
+        group_name = group_response[0]["name"]
+        r.set(f"groups:names:{group_id}", group_name, ex=604800)
         return group_response[0]["name"]
 
+    cached_name = r.get(f"users_names:{author_id}")
+    if cached_name:
+        return cached_name
     users_response = safe_vk_call(vk.users.get, user_ids=author_id)
     if not users_response:
         return "Неизвестный автор"
     user = users_response[0]
     first_name = user.get("first_name")
     last_name = user.get("last_name")
-    return f"{first_name} {last_name}"
-
-
-def update_latest_comments(latest_saved_posts, new_comments):
-    logger.info("Started latest_comments update")
-    for post_with_new_comments in new_comments:
-        post_id = post_with_new_comments["post_id"]
-        latest_new_comment_id = post_with_new_comments["new_comments"][-1][
-            "comment_id"
-        ]
-        if post_id in latest_saved_posts:
-            latest_saved_posts[post_id]["comment_id"] = latest_new_comment_id
-        else:
-            latest_saved_posts[post_id] = {"comment_id": latest_new_comment_id}
-    logger.info("Finished latest_comments update")
-    return latest_saved_posts
+    user_name = f"{first_name} {last_name}"
+    r.set(f"users:names:{author_id}", user_name, ex=604800)
+    return user_name
