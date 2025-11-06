57a93950a2888d73a3b332fa0686d0b45d98221e
toro <timofey89rus@gmail.com>
2025-11-06 19:16:33 +0300

    change vk_api dependency to vk module

---
+++ src/vk.py
@@ -0,0 +1,87 @@
+import requests
+
+from config.settings import VK_TOKEN, VK_GROUP_ID
+
+VK_URL = "https://api.vk.ru/method"
+
+API_VERSION = "5.199"
+
+
+def make_vk_call(method: str, *args, **params) -> dict:
+    URL = f"{VK_URL}/{method}"
+    params["access_token"] = VK_TOKEN
+    params["v"] = API_VERSION
+    for key, value in list(params.items()):
+        if isinstance(value, (list, tuple, set)):
+            params[key] = ",".join(map(str, value))
+    try:
+        r = requests.get(URL, params=params)
+        vk_response = r.json()
+    except Exception as exc:
+        print(f"Exception - {exc}")
+
+    return vk_response
+
+
+def get_posts(group_id=VK_GROUP_ID, count=20):
+    api_response = make_vk_call("wall.get", owner_id=group_id, count=count)
+    if not api_response:
+        return []
+    return api_response["response"]["items"]
+
+
+def get_comments(post_id, last_comment_id, group_id=VK_GROUP_ID, count=100):
+    api_response = make_vk_call(
+        "wall.getComments",
+        owner_id=group_id,
+        post_id=post_id,
+        count=count,
+        sort="asc",
+        start_comment_id=last_comment_id,
+    )
+    if not api_response:
+        return []
+    comments = api_response["response"]["items"]
+    if last_comment_id:
+        comments = comments[1:]
+    return comments
+
+
+def get_user_name(user_id):
+    api_response = make_vk_call("users.get", user_ids=user_id)
+    if not api_response:
+        return "Неизвестный пользователь"
+    user = api_response["response"][0]
+    first_name = user.get("first_name")
+    last_name = user.get("last_name")
+    user_name = f"{first_name} {last_name}"
+    return user_name
+
+
+def get_group_name(group_id):
+    api_response = make_vk_call("groups.getById", group_id=group_id)
+    if not api_response:
+        return "Неизвестное сообщество"
+    group_name = api_response["response"]["groups"][0]["name"]
+    return group_name
+
+
+def get_users_names(users_id):
+    api_response = make_vk_call("users.get", user_ids=users_id)
+    if not api_response:
+        return {}
+    users = api_response["response"]
+    return {
+        user["id"]: f"{user.get("first_name")} {user.get("last_name")}"
+        for user in users
+    }
+
+
+def get_groups_names(group_ids):
+    api_response = make_vk_call("groups.getById", group_ids=group_ids)
+    if not api_response:
+        return {}
+    groups = api_response["response"]["groups"]
+    return {group["id"]: group["name"] for group in groups}
+
+
--- src/vk_comments_parser.py
+++ src/vk_comments_parser.py
@@ -1,49 +1,31 @@
 from datetime import date, datetime, timedelta
 
-import vk_api
-from vk_api.exceptions import VkApiError
-
-from config.settings import VK_GROUP_ID, VK_TOKEN
+import vk
 from src.logger import logger
 from src.redis_setup import r
 
 logger = logger.getChild(__name__)
 
-vk_session = vk_api.VkApi(token=VK_TOKEN)
-vk = vk_session.get_api()
-
 
 def get_new_comments():
     logger.info("Started new comments collecting")
     recent_posts = get_recent_posts_with_comments()
-    new_comments = []
+    posts_with_new_comments = []
     for post in recent_posts:
-        post_new_comments = collect_new_comments_for_post(post)
-        if post_new_comments:
-            new_comments.append(post_new_comments)
-    logger.info(f"Collected {len(new_comments)} posts with new comments")
-    return new_comments
+        if post_new_comments := collect_new_comments_for_post(post):
+            posts_with_new_comments.append(post_new_comments)
+    if posts_with_new_comments:
+        users_ids, groups_ids = collect_author_ids(posts_with_new_comments)
+        posts_with_new_comments = add_authors_names(
+            posts_with_new_comments, users_ids, groups_ids
+        )
+    return posts_with_new_comments
 
 
-def safe_vk_call(method, *args, **kwargs):
-    try:
-        return method(*args, **kwargs)
-    except VkApiError as exc:
-        logger.error(f"VK Api Error: {exc} for {method} with {args, kwargs}")
-    except Exception as exc:
-        logger.error(f"Unexpected error: {exc}.Traceback -", exc_info=True)
-    return None
-
-
-def get_recent_posts_with_comments(
-    group_id: int = VK_GROUP_ID, days: int = 7
-) -> list:
+def get_recent_posts_with_comments(days: int = 7) -> list:
     end_date = date.today()
     start_date = end_date - timedelta(days=days)
-    posts_response = safe_vk_call(vk.wall.get, owner_id=group_id, count=20)
-    if not posts_response:
-        return []
-    posts = posts_response["items"]
+    posts = vk.gqet_posts()
     recent_posts_with_comments = []
     for post in posts:
         post_date = date.fromtimestamp(post["date"])
@@ -53,25 +35,13 @@ def get_recent_posts_with_comments(
     return recent_posts_with_comments
 
 
-def collect_new_comments_for_post(post, group_id=VK_GROUP_ID):
+def collect_new_comments_for_post(post):
     post_id = post["id"]
-    post_text = f"{post["text"][:100]}..."
+    post_text = f"{post['text'][:100]}..."
     post_date = date.fromtimestamp(post["date"])
     post_new_comments = []
-    last_saved_comment_id = int(r.get(f"post:{post_id}:last_comment_id:") or 0)
-    post_comments_response = safe_vk_call(
-        vk.wall.getComments,
-        owner_id=group_id,
-        post_id=post_id,
-        count=100,
-        sort="asc",
-        start_comment_id=last_saved_comment_id,
-    )
-    if not post_comments_response:
-        return []
-    post_comments = post_comments_response["items"]
-    if last_saved_comment_id and post_comments:
-        post_comments = post_comments[1:]
+    last_comment_id = int(r.get(f"post:{post_id}:last_comment_id") or 0)
+    post_comments = vk.get_comments(post_id, last_comment_id)
     for comment in post_comments:
         new_comment = process_comment(comment)
         if new_comment:
@@ -93,43 +63,45 @@ def collect_new_comments_for_post(post, group_id=VK_GROUP_ID):
 def process_comment(comment):
     comment_text = comment["text"]
     if comment_text:
-        comment_author = get_author(comment["from_id"])
         comment_time = datetime.fromtimestamp(comment["date"])
         return {
             "comment_id": comment["id"],
             "comment_time": comment_time,
             "text": comment_text,
-            "author": comment_author,
             "author_id": comment["from_id"],
         }
 
 
-def get_author(author_id):
-    if author_id < 0:
-        return get_group_name(author_id)
-
-    cached_name = r.get(f"user:{author_id}:name")
-    if cached_name:
-        return cached_name
-    users_response = safe_vk_call(vk.users.get, user_ids=author_id)
-    if not users_response:
-        return "Неизвестный автор"
-    user = users_response[0]
-    first_name = user.get("first_name")
-    last_name = user.get("last_name")
-    user_name = f"{first_name} {last_name}"
-    r.set(f"user:{author_id}:name", user_name, ex=604800)
-    return user_name
+def collect_author_ids(new_comments):
+    users_ids, groups_ids = set(), set()
+    for post in new_comments:
+        for comment in post["new_comments"]:
+            author_id = comment["author_id"]
+            if author_id > 0:
+                users_ids.add(author_id)
+            else:
+                groups_ids.add(abs(author_id))
+    return users_ids, groups_ids
 
 
-def get_group_name(id):
-    group_id = abs(id)
-    cached_name = r.get(f"group:{group_id}:name")
-    if cached_name:
-        return cached_name
-    group_response = safe_vk_call(vk.groups.getById, group_id=group_id)
-    if not group_response:
-        return "Неизвестное сообщество"
-    group_name = group_response[0]["name"]
-    r.set(f"group:{group_id}:name:", group_name, ex=604800)
-    return group_response[0]["name"]
+def add_authors_names(posts, users_ids, groups_ids):
+    id_to_name = {}
+
+    if users_ids:
+        users = vk.get_users_names(users_ids)
+        id_to_name.update(users)
+
+    if groups_ids:
+        groups = vk.get_groups_names(groups_ids)
+        id_to_name.update(groups)
+
+    posts_with_names = posts
+    for post in posts:
+        for comment in post["new_comments"]:
+            comment["author_name"] = id_to_name.get(
+                comment["author_id"], "Неизвестный автор"
+            )
+    return posts_with_names
+
+
+print(get_new_comments())
