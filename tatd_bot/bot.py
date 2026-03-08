61553306b688afac915a9a2d2858da5a5c285850
toro <timofey89rus@gmail.com>
2026-03-08 14:44:25 +0300

    fix comments monitor dns resolution error(hotfix)

--- tatd_bot/bot.py
+++ tatd_bot/bot.py
@@ -6,9 +6,18 @@ from tatd_bot.tg.bot_runner import dp, monitor_vk_comments, tatd_bot
 logger = logger.getChild(__name__)
 
 
+async def monitor_vk_comments_safe():
+    while True:
+        try:
+            await monitor_vk_comments()
+        except Exception:
+            logger.exception("VK monitor crashed")
+            await asyncio.sleep(60 * 5)
+
+
 async def run_bot():
     logger.info("Bot started")
-    asyncio.create_task(monitor_vk_comments())
+    asyncio.create_task(monitor_vk_comments_safe())
     await dp.start_polling(tatd_bot)
 
 
